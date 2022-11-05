e18b5324ee97f380b59f1ca81818c70b05dab2c1
Felipe Fardo <felipe.fardo@hotmail.com.br>
2022-11-05 17:35:49 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "NOTÍCIA 01",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    "photoCover": "https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png"
+  },
+  {
+    "id": "2",
+    "title": "NOTÍCIA 02",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    "photoCover": "https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png"
+  },
+  {
+    "id": "3",
+    "title": "NOTÍCIA 03",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    "photoCover": "https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png"
+  },
+  {
+    "id": "4",
+    "title": "NOTÍCIA 04",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    "photoCover": "https://s3-alpha.figma.com/hub/file/948140848/1f4d8ea7-e9d9-48b7-b70c-819482fb10fb-cover.png"
+  },
+];
